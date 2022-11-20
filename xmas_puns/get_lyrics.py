7a0cc7c10685d03f1f276bf92e23deaf8abdfc8f
Giacomo Nanni <gn.nanni@gmail.com>
2022-11-20 17:07:36 +0000

    fix api request

--- xmas_puns/get_lyrics.py
+++ xmas_puns/get_lyrics.py
@@ -4,6 +4,7 @@ import time
 import json
 import os
 import configparser
+import re
 
 # Get configuration file
 config = configparser.ConfigParser()
@@ -13,31 +14,30 @@ token = config.get('main', 'secret')
 
 genius = Genius(token)
 
-df = pandas.read_csv("data/top100.csv")
+df = pandas.read_csv("data/top100.csv")[:5]
 data = []
-
 
 for idx,row in df.iterrows():
     title = row["Title"].replace('"', '')
     artist = row["Artist"]
     try:
-        song = genius.search_song(title, artist)
+        song = genius.search_song(title)
+        
         clean_lyrics = song.lyrics.split("Lyrics")[1]
         clean_lyrics = clean_lyrics.split("Embed")[0]
+        clean_lyrics = "".join(re.split("\(|\)|\[|\]", clean_lyrics)[::2])
 
         data.append(
             {
-                'artist':artist,
+                'artist':song.artist,
                 'title':title,
                 'lyrics':clean_lyrics
             }
         )
-        time.sleep(1)
-
+        #time.sleep(.30)
+        
     except Exception as e:
         print(e)
 
-with open('out/data.json', 'w') as f:
-    json.dump(data, f)
-
-
+with open('data/data.json', 'w') as f:
+    json.dump(data, f, ensure_ascii=False)
